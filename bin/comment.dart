a9011d51ab6d7704de6102bcaf977f53d1e11a07
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-27 13:02:41 +0700

    Add files via upload

---
+++ bin/comment.dart
@@ -0,0 +1,16 @@
+/// ini komentar documentation
+/// this is main function
+/// will executed by dart
+
+void main() {
+  // ini komentar single line
+  // this is name variable
+  var name = 'Riza Rumayanti Dewi';
+
+  /**
+   * this is multi line comment
+   * you can add more here
+   */
+
+  print(name);
+}
