80b90bb5bebb80e0d8cf06230be375e6139daf87
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-28 15:24:28 +0700

    Update map.dart

--- bin/map.dart
+++ bin/map.dart
@@ -1,5 +1,6 @@
 void main() {
-  Map<String, String> map1 = {};
+  
+  Map<String, String> map1 = {};              //membuat map
   var map2 = Map<String, String>();
   var map3 = <String, String>{};
 
@@ -14,15 +15,16 @@ void main() {
   name['last'] = 'Dewi';
 
   print(name);
-  print(name['first']);
+  print(name.length);                         //panjang map
+  print(name['first']);                       //mendapatkan data di map
 
-  name['middle'] = 'Gilang';
+  name['middle'] = 'Gilang';                  //mengubah data di map
   print(name);
 
-  name.remove('last');
+  name.remove('last');                        //menghapus data di map
   print(name);
 
-  var names = <String, String>{
+  var names = <String, String>{               //deklarasi map secara langsung
     'first': 'Gilang',
     'last': 'Permana',
   };
