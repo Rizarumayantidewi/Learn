d4033f00ca9f37eb09138629384ffc48ecd87741
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-28 14:37:33 +0700

    Update string.dart

--- bin/string.dart
+++ bin/string.dart
@@ -1,22 +1,32 @@
 void main() {
+  
+  //string
   String firstName = 'Riza';
   String lastName = "Rumayanti Dewi";
 
   print(firstName);
   print(lastName);
 
-  var fullName = '$firstName ${lastName}';
+  
+  // string interpolaation
+  var fullName = '$firstName ${lastName}';        //ini expression
   print(fullName);
 
-  var text = 'this is \' dart \' \$cool';
+  
+  //karakter backslah
+  var text = 'this is \' dart \' \$cool';         //yg ini
   print(text);
 
-  var name1 = firstName + " " + lastName;
-  var name2 = 'Riza' ' Rumayanti' ' Dewi';
+  
+  //menggabungkan string
+  var name1 = firstName + " " + lastName;         //cara 1
+  var name2 = 'Riza' ' Rumayanti' ' Dewi';        //cara 2
 
   print(name1);
   print(name2);
 
+  
+  //multilnie string
   var longString = '''
   this is long string
   multiline string
