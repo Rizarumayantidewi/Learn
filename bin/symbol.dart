1f3c36c23b0079662a638cd8bdcb5e4b786c8236
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-28 15:28:45 +0700

    Update symbol.dart

--- bin/symbol.dart
+++ bin/symbol.dart
@@ -1,7 +1,10 @@
 void main() {
-  Symbol symbol1 = Symbol("Riza Rumayanti Dewi");
-  var symbol2 = #belajar_dart;
+
+  //simbol bisa digunakan sbg constant
+  Symbol symbol1 = Symbol("Riza Rumayanti Dewi");         //cara 1
+  var symbol2 = #belajar_dart;                            //cara 2
 
   print(symbol1);
   print(symbol2);
+  
 }
