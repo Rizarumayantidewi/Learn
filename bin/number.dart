bf28460075d3071c640d6f26ae014847d5663bb9
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-28 14:31:57 +0700

    Update number.dart

--- bin/number.dart
+++ bin/number.dart
@@ -1,10 +1,14 @@
 void main() {
+  
+  //tipedata number
   int number1 = 11;
-  double number2 = 8.8;
+  double number2 = 8.8;       //desimal pake .
 
   print(number1);
   print(number2);
 
+  
+  //tipe data num
   num number = 1.1;
   print(number);
 
