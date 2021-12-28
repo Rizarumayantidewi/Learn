9a546a5f0ad5736d02bc386735b3d8b793a89a7f
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-28 14:13:05 +0700

    Create for.dart

---
+++ bin/for.dart
@@ -0,0 +1,26 @@
+void main() {
+  //perulangan tanpa henti
+  //for (;;) {
+  //  print('Perulangan Tanpa Henti');
+  //}
+
+  //perulangan dengan kondisi
+  var counter = 1;
+
+  for (; counter <= 8;) {
+    print('Perulangan ke - $counter');
+
+    counter++;
+  }
+
+  //init statement
+  //for(var counter = 1; counter <= 8 ;){     //ini init statement nya
+  //  print('Perilangan ke - $counter');
+  //  counter++
+  // }
+
+  //post statement
+  //for (var counter = 1; counter <= 8; counter++) {    //ini post statement nya
+  //  print('Perulangan ke - $counter');
+  // }
+}
