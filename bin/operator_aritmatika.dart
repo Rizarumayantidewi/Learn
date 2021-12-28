22b7dea2c40e81f08e9201dcea1bbbdc0581b4ed
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-28 14:45:02 +0700

    Update operator_aritmatika.dart

--- bin/operator_aritmatika.dart
+++ bin/operator_aritmatika.dart
@@ -2,10 +2,10 @@ void main() {
   var first = 11;
   var second = 8;
 
-  print(first + second);
-  print(first - second);
-  print(first * second);
-  print(first / second);
-  print(first ~/ second);
-  print(first % second);
+  print(first + second);        //pertambahan
+  print(first - second);        //- maka pengurangan jika -1 maka negatif
+  print(first * second);        //perkalian
+  print(first / second);        //pembagian hasilnya double
+  print(first ~/ second);       //pembagian hasilnya int
+  print(first % second);        //sisa bagi
 }
