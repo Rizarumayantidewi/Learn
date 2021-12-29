16180bc357a3e0a812f302d239d75b44ab8985fc
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-29 21:25:43 +0700

    Update initializing_formal_parameter.dart

--- bin/oop/initializing_formal_parameter.dart
+++ bin/oop/initializing_formal_parameter.dart
@@ -5,23 +5,6 @@ class Person {
 
   //ini ifp
   Person(this.name, this.address) {}
-
-  //ini named constructor
-  //bisa membuat constructor lebih dr satu wajib dgn nama yg berbeda
-  Person.withName(this.name) {}
-
-  Person.withAddress(this.address) {}
 }
 
-//pada parameter bs lsg sbtkn field yg akan diubah
-
-//manggil named constuctor
-void main() {
-  var person = Person.withName("Riza");
-  var person2 = Person.withAddress("Garut");
-  var person3 = Person("Gilang", "Sukabumi");
-
-  print(person);
-  print(person2);
-  print(person3);
-}
+// parameter bs lsg sbtkn field yg akn diubah
