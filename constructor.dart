4818dfddd7ada4d9fa076b55df0df6ae9d11d1b4
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-29 20:10:55 +0700

    Add files via upload

---
+++ constructor.dart
@@ -0,0 +1,23 @@
+//method yg dipanggil saat pertama kali object dibuat
+
+class Person {
+  String name = "Riza";
+  String? address;
+  final String country = "Indonesia";
+
+  Person(String paramName, String paramAddress) {
+    name = paramName;
+    address = paramAddress;
+  }
+
+  void sayHello(String paramName) {
+    print("Hello $paramName, My Name is $name");
+  }
+}
+
+void main() {
+  var person = Person("Riza", "Sukabumi");
+
+  person.name = "Gilang";
+  person.sayHello('Halley');
+}
