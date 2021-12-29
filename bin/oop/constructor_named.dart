d6dd4328b1897c04f38c9e9828f11546d300daee
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-29 21:25:14 +0700

    Update constructor_named.dart

--- bin/oop/constructor_named.dart
+++ bin/oop/constructor_named.dart
@@ -1,3 +1,23 @@
-import 'method.dart';
+//ini named constructor
+//bisa membuat constructor lebih dr satu wajib dgn nama yg berbeda
+class Person {
+  String name = "Riza";
+  String? address;
+  final String country = "Indonesia";
+  Person(this.name, this.address) {}
 
-Person(this)
+  Person.withName(this.name) {}
+
+  Person.withAddress(this.address) {}
+}
+
+//manggil named constuctor
+void main() {
+  var person = Person.withName("Riza");
+  var person2 = Person.withAddress("Garut");
+  var person3 = Person("Gilang", "Sukabumi");
+
+  print(person);
+  print(person2);
+  print(person3);
+}
