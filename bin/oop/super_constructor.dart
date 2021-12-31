bc626bac333a084a64afc47c2534d76a9e083d13
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-31 22:15:02 +0700

    Update super_constructor.dart

--- bin/oop/super_constructor.dart
+++ bin/oop/super_constructor.dart
@@ -11,3 +11,11 @@ class Manager {
 class VicePresident extends Manager {
   VicePresident(String name) : super(name) {}
 }
+
+void main() {
+  var manager = Manager("Riza");
+  manager.sayHello('Kai');
+
+  var vp = VicePresident("Gilang");
+  vp.sayHello('Gilang');
+}
