e2660fc6f3c1b37ba48664c82308ee9ad21d7392
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2022-02-17 21:40:23 +0700

    Add files via upload

---
+++ bin/oop/object_class.dart
@@ -0,0 +1,11 @@
+// co adlh superclass untuk smua class
+
+class Person {}
+
+void main() {
+  var number = 100;
+  print(number.toString());
+
+  var person = Person();
+  print(person.toString());
+}
